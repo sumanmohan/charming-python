36e66e6ded5e29b5ef51e0527d1091db79aff82d
Suman Rajmohan <sumanrajmohan@Sumans-MacBook-Pro.local>
2015-05-22 10:05:51 -0400

    Suman: Making log write to a file in addition to stream

--- src/logging/log.py
+++ src/logging/log.py
@@ -3,13 +3,21 @@ __author__ = 'sumanrajmohan'
 import logging
 
 def setup_custom_logger(name):
-    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+    logPath = 'out/'
+    fileName = 'simple_log'
+
+    logFormatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
     handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
+    handler.setFormatter(logFormatter)
 
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
+
+    fileHandler = logging.FileHandler("{0}/{1}.log".format(logPath, fileName))
+    fileHandler.setFormatter(logFormatter)
+    logger.addHandler(fileHandler)
 
     return logger
